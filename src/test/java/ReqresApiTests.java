8274b0193eb190032c4634f1593a701a31135535
Sanovski44 <sanovski44@gmail.com>
2021-09-15 19:50:27 +0300

    Added successful and unsuccessful tests

---
+++ src/test/java/ReqresApiTests.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresApiTests {
+    @Test
+    void successfulLogin() {
+        given()
+                .contentType(JSON)
+                .body("{\"email\": \"eve.holt@reqres.in\"," +
+                        "\"password\": \"cityslicka\"}")
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unsuccessfulLogin() {
+        given()
+                .contentType(JSON)
+                .body("{\"email\": \"peter@klaven\"}")
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+}
